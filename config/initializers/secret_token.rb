be4c4a7c29869b8e1d2f95ebe406c68b48815f30
ChengDuo <cd19900419@hotmail.com>
2013-11-20 20:46:35 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tientrance::Application.config.secret_key_base = 'b1813b017eb029a74b3c8b7f62d35b8efd4fef1a36a5e9118af543708466a0b3a89f2075a022701a48fd3653c5f45b4b5927d532343f5cd053b5f5f5d56d7fd2'
